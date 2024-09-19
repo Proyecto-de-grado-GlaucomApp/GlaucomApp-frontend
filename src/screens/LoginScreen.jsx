500f3f6246c08d962f55a648e75e37992edf2269
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-09-18 20:26:52 -0500

    login screen

---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import PrimaryButton from '../components/shared/PrimaryButton';
+import FormikInputField from '../components/login/FormikInputField';
+import Header from "../components/shared/Header";
+import TextLink from "../components/login/TextLink";
+import {Formik} from 'formik';
+import {loginSchema} from '../validationSchemas/login';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+const initialValues   = {
+    email: '',
+    password: '',
+};
+
+export default function LoginScreen({navigation}) {
+
+
+    return (
+        <Formik validationSchema={loginSchema} initialValues={initialValues} onSubmit={values => console.log(values)}>
+            {({handleSubmit}) => {
+                return (
+                    <View style={styles.container}>
+                        <Header />
+
+                        <FormikInputField
+                            name='email'
+                            placeholder='Email'
+                            iconName="person"
+                        />
+                        <FormikInputField
+                            name='password'
+                            placeholder='Password'
+                            iconName="eye"
+                            secureTextEntry={true}
+                        />
+
+                        <PrimaryButton title="Iniciar Sesión" onPress={() => navigation.navigate('Home')} />
+                        <TextLink title="Registrarse" onPress={() => navigation.navigate('Login')} />
+
+                    </View>
+                );
+            }}
+        </Formik>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#769BCE',
+        padding: 20,
+        justifyContent: 'center',
+    },
+});
+
+
