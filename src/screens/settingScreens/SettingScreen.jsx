2e2b4ec40f63bbf83fc3e5654dd2fe74e287970e
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-10-23 13:40:45 -0500

    Component Settings

--- src/screens/settingScreens/SettingScreen.jsx
+++ src/screens/settingScreens/SettingScreen.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useBackHome} from "../../hooks/useBackHome";
+import PrimaryButton from "../../components/shared/PrimaryButton";
 
 const SettingScreen = ({ navigation }) => {
 
     useBackHome(navigation);
 
     return (
-        <View>
-            <Text>Settings</Text>
+        <View style={styles.container}>
+            <PrimaryButton title="Cerrar Sesión" />
         </View>
     );
 }
 
 export default SettingScreen;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+});
