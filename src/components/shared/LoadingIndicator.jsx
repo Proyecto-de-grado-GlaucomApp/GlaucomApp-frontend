b786bc9c8887bf2d39f45963b7dbbf4fc08676a8
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-10-24 22:59:50 -0500

    color indicator

--- src/components/shared/LoadingIndicator.jsx
+++ src/components/shared/LoadingIndicator.jsx
@@ -3,7 +3,7 @@ import { View, ActivityIndicator, Text, StyleSheet } from 'react-native';
 
 const LoadingIndicator = ({ message }) => (
     <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#769BCE" />
         <Text>{message}</Text>
     </View>
 );
